87c219a118a6c98cec43f8c1c1e4494c437770e1
deriya-a <derya0806@gmail.com>
2023-08-20 17:27:16 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,3 +5,4 @@ Rails.application.routes.draw do
   #resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+end
