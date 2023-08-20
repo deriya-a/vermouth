19badf67e20896c0068ca443f4fc0d064294d7ac
deriya-a <derya0806@gmail.com>
2023-08-20 16:33:44 +0900

    Update user model
    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: '半角英数字混合で入力してください' }
+  validates :nickname, presence: true
+
+  has_many :items
+  has_many :coordinates
 end
