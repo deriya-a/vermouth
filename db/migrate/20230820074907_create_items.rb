7a4d4136631add143d71790fdc81ab952d5d2997
deriya-a <derya0806@gmail.com>
2023-08-21 20:01:21 +0900

    Update 20230820074907_create_items.rb
    add items colums

--- db/migrate/20230820074907_create_items.rb
+++ db/migrate/20230820074907_create_items.rb
@@ -1,7 +1,11 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
-    create_table :items do |t|
-
+    create_table   :items do |t|
+      t.integer    :category_id, null: false
+      t.integer    :color_id, null: false
+      t.integer    :type_id, null: false
+      t.text       :item_memo
+      t.references :user ,              null: false, foreign_key: true
       t.timestamps
     end
   end
