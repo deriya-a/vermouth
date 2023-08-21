c8e99c32706adc7af3294d6d3908a26f571cd07f
deriya-a <derya0806@gmail.com>
2023-08-21 19:55:21 +0900

    Create category.rb
    active hush

---
+++ app/models/category.rb
@@ -0,0 +1,19 @@
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: "---" },
+    { id: 2, name: "留袖" },
+    { id: 3, name: "訪問着" },
+    { id: 4, name: "付け下げ" },
+    { id: 5, name: "色無地" },
+    { id: 6, name: "小紋" },
+    { id: 7, name: "紬" },
+    { id: 8, name: "御召" },
+    { id: 9, name: "浴衣" },
+    { id: 10, name: "袋帯" },
+    { id: 11, name: "名古屋帯" },
+    { id: 12, name: "半幅帯" },
+    { id: 13, name: "帯揚げ" },
+    { id: 14, name: "帯締め" },
+    { id: 15, name: 'その他' }
+  ]
+  end
