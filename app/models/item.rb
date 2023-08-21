24d2854e968e8bdf4550af8a0ced46ff09bd194f
deriya-a <derya0806@gmail.com>
2023-08-21 20:10:06 +0900

    Update item.rb
    added validation for active hush

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,9 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :color
   belongs_to :type
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :color_id, numericality: { other_than: 1 } 
+
 end
