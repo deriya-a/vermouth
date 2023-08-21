b061eca1a1d1b93d8aedca846e9ee715bf59d14f
deriya-a <derya0806@gmail.com>
2023-08-21 21:41:08 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,10 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:category_id, :color_id, :type_id, :item_memo, :image).merge(user_id: current_user.id)
+  end
 end
