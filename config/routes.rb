109ecabb88c1a84e54932f26d485ef7d1c7e04c9
deriya-a <derya0806@gmail.com>
2023-08-21 15:31:20 +0900

    Update landing.html
    add registration/login path to buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "items#index"
+  root to: "landings#index"
   resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
   #resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
